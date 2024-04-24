7f3c61946beba963154c677006bcce92cd0c2f36
bwjennings <benjennings@studioscience.com>
2024-04-24 17:30:24 -0400

    new intro structure

--- index.js
+++ index.js
@@ -2,15 +2,38 @@ function openCity(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("nav-card");
+    tablinks = document.getElementsByClassName("nav-card");  // Ensure this matches your tab button class name
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "flex";
+    var activeContent = document.getElementById(cityName);
+    activeContent.style.display = "flex";
+    activeContent.scrollTop = 0;  // Set scroll position to the top
     evt.currentTarget.className += " active";
-  }
+}
+
   
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
+
+
+  var toggle = document.getElementById("theme-toggle");
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
