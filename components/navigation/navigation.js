945e0a3cf6527b09300ba07fd0c3a447ead782d9
bwjennings <benjennings@studioscience.com>
2024-10-04 16:25:01 -0400

    Update navigation.js

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -10,38 +10,38 @@ class CustomSidebar extends HTMLElement {
     // Create the template without relying on attributes
     const template = document.createElement("template");
     template.innerHTML = `
-    <link  href="css/style.css" rel="stylesheet"/>
-      <link  href="components/navigation/navigation.css" rel="stylesheet"/>
-      
-      <nav class="sidebar">
-        <h2 class="site-title"><a href="index.html">Ben</a></h2>
-        <site-settings></site-settings>
-        <menu>
-          <li><a tabindex="1" class="nav-card item1" href="index.html">
-              <span class="icon">waving_hand</span>
-              <span class="title">Home</span>
-          </a></li>
-          <li><a tabindex="2" class="nav-card item2" href="fundamentals.html">
-              <span class="icon">psychology</span>
-              <span class="title">Fundamentals</span>
-          </a></li>
-          <li><a tabindex="3" class="nav-card item3" href="designs.html">
-              <span class="icon">web</span>
-              <span class="title">Designs</span>
-          </a></li>
-          <li><a tabindex="4" class="nav-card item4" href="experiments.html">
-              <span class="icon">experiment</span>
-              <span class="title">Experiments</span>
-          </a></li>
-          <li><a tabindex="5" class="nav-card item5" href="resources.html">
-              <span class="icon">folder_open</span>
-              <span class="title">Resources</span>
-          </a></li>
-        </menu>
-        <theme-slider class="quick-theme" data-hide-label></theme-slider>
-      </nav>
+    <link href="css/style.css" rel="stylesheet"/>
+    <link href="components/navigation/navigation.css" rel="stylesheet"/>
+    
+    <nav class="sidebar">
+      <h2 class="site-title"><a href="index.html">Ben</a></h2>
+      <site-settings></site-settings>
+      <menu>
+        <li><a tabindex="1" class="nav-card item1" href="index.html">
+            <span class="icon">waving_hand</span>
+            <span class="title">Home</span>
+        </a></li>
+        <li><a tabindex="2" class="nav-card item2" href="fundamentals.html">
+            <span class="icon">psychology</span>
+            <span class="title">Fundamentals</span>
+        </a></li>
+        <li><a tabindex="3" class="nav-card item3" href="designs.html">
+            <span class="icon">web</span>
+            <span class="title">Designs</span>
+        </a></li>
+        <li><a tabindex="4" class="nav-card item4" href="experiments.html">
+            <span class="icon">experiment</span>
+            <span class="title">Experiments</span>
+        </a></li>
+        <li><a tabindex="5" class="nav-card item5" href="resources.html">
+            <span class="icon">folder_open</span>
+            <span class="title">Resources</span>
+        </a></li>
+      </menu>
+      <theme-slider class="quick-theme" data-hide-label></theme-slider>
+    </nav>
     `;
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.append(template.content.cloneNode(true));
 
     // Bind methods to ensure correct 'this' context
     this.handleNavigation = this.handleNavigation.bind(this);
@@ -61,28 +61,30 @@ class CustomSidebar extends HTMLElement {
   }
 
   addEventListeners() {
-    // Attach event listeners to navigation links
-    this.shadowRoot.querySelectorAll(".nav-card").forEach((element) => {
-      element.addEventListener("click", this.handleNavigation);
-    });
+    // Attach event listener to the parent 'menu' element for event delegation
+    const menu = this.shadowRoot.querySelector("menu");
+    if (menu) {
+      menu.addEventListener("click", this.handleNavigation);
+    }
   }
 
   removeEventListeners() {
-    // Detach event listeners
-    this.shadowRoot.querySelectorAll(".nav-card").forEach((element) => {
-      element.removeEventListener("click", this.handleNavigation);
-    });
+    // Detach event listener from the parent 'menu' element
+    const menu = this.shadowRoot.querySelector("menu");
+    if (menu) {
+      menu.removeEventListener("click", this.handleNavigation);
+    }
   }
 
   handleNavigation(event) {
-    // Prevent default link behavior if custom navigation is desired
-    event.preventDefault();
-
-    // Get the href attribute from the clicked link
-    const href = event.currentTarget.getAttribute("href");
-    if (href) {
-      // Navigate to the new URL
-      window.location.href = href;
+    // Only handle clicks on '.nav-card' elements
+    const target = event.target.closest(".nav-card");
+    if (target) {
+      event.preventDefault();
+      const href = target.getAttribute("href");
+      if (href) {
+        window.location.href = href;
+      }
     }
   }
 
